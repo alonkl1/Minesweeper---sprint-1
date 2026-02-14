ccc53c0f8a8f4517a9d12c267c743591170b7b15
alonkl1 <122540417+alonkl1@users.noreply.github.com>
2026-02-14 09:31:22 +0200

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -1,169 +1,69 @@
 'use strict'
 
-// const WALL = '&#8251;'
-// const FOOD = '&middot;'
-// const SUPERFOOD = '&#169;'
-// const EMPTY = ' '
-
-const MINE_IMG='<img src="img/flag.png" alt="mine">'
-const BOMB_IMG='<img src="img/bomb.png" alt="bomb">'
+const WALL = '&#8251;'
+const FOOD = '&middot;'
+const SUPERFOOD = '&#169;'
+const EMPTY = ' '
 
 const gGame = {
+    score: 0,
     isOn: false,
-    revealedCount: 0,
-    markedCount: 0,
-    secsPassed: 0
+    foodCount: 0
 }
-
-const gLevel = {
-    SIZE: 4,
-    MINES: 2
-}
-
-
-var gFirstclick
 var gBoard
 
-
-function onInit() {
-
+function init() {
     console.log('hello')
 
-    const elModal = document.querySelector(".modal")
+    const elModal = document.querySelector(".modal ")
     elModal.style.display = 'none'
-    // const elWin = document.querySelector(".win")
-    // elWin.style.display = 'none'
-
-    gFirstclick = true
+    const elWin = document.querySelector(".win ")
+    elWin.style.display = 'none'
 
     gGame.score = 0
 
-    gBoard = buildBoard()
-    console.table(gBoard)
 
+
+    gBoard = buildBoard()
+
+    updateScore(0)
+
+    createPacman(gBoard)
+    createGhosts(gBoard)
 
     renderBoard(gBoard, '.board-container')
     gGame.isOn = true
 }
 
 function buildBoard() {
-    const size = gLevel.SIZE
+    const size = 10
     const board = []
 
     for (var i = 0; i < size; i++) {
         board.push([])
 
         for (var j = 0; j < size; j++) {
-            board[i][j] = createCell()
+            board[i][j] = FOOD
+
+            if (i === 0 || i === size - 1 ||
+                j === 0 || j === size - 1 ||
+                (j === 3 && i > 4 && i < size - 2)) {
+                board[i][j] = WALL
+            }
+
+            else if (i === 1 && j === 1 ||
+                i === 1 && j === size - 2 ||
+                i === size - 2 && j === 1 ||
+                i === size - 2 && j === size - 2)
+                board[i][j] = SUPERFOOD
+
+            else {
+                gGame.foodCount++
+            }
         }
     }
-    // var i = 0
-    // while (i < gLevel.MINES) {
-    //     var idxI = getRandomIntInclusive(0, board.length - 1)
-    //     var idxJ = getRandomIntInclusive(0, board[0].length - 1)
-    //     const currCell = board[idxI][idxJ]
-    //     if (currCell.isMine) continue
-    //     currCell.isMine = true
-    //     i++
-    // }
-
-    // setMinesNegsCount(board)
-
-
+    gGame.foodCount--
     return board
-}
-
-function placeMines(board, i, j) {
-
-    var loopCounter = 0
-
-    while (loopCounter < gLevel.MINES) {
-        var idxI = getRandomIntInclusive(0, board.length - 1)
-        var idxJ = getRandomIntInclusive(0, board[0].length - 1)
-        const currCell = board[idxI][idxJ]
-        if (currCell.isMine || idxI === i && idxJ === j) continue
-        currCell.isMine = true
-        loopCounter++
-    }
-
-    // board[1][1].isMine=true
-    // board[2][3].isMine=true
-    gFirstclick = false
-    setMinesNegsCount(board)
-
-
-
-}
-
-function setMinesNegsCount(board) {
-    for (var i = 0; i < board.length; i++) {
-        for (var j = 0; j < board[i].length; j++) {
-            const currCell = board[i][j]
-
-            if (currCell.isMine) continue
-
-            currCell.minesAroundCount = countMinesAround(board, i, j)
-        }
-    }
-}
-
-function countMinesAround(board, idxI, idxJ) {
-    var count = 0
-    for (var i = idxI - 1; i <= idxI + 1; i++) {
-
-        if (i < 0 || i >= board.length) continue
-
-        for (var j = idxJ - 1; j <= idxJ + 1; j++) {
-
-            if (j < 0 || j >= board[i].length) continue
-
-            if (i === idxI && j === idxJ) continue
-
-            if (board[i][j].isMine) count++
-        }
-    }
-    return count
-}
-
-function expandReveal(board, elCell, idxI, idxJ) {
-    for (var i = idxI - 1; i <= idxI + 1; i++) {
-
-        if (i < 0 || i >= board.length) continue
-
-        for (var j = idxJ - 1; j <= idxJ + 1; j++) {
-
-            if (j < 0 || j >= board[i].length) continue
-
-            if (i === idxI && j === idxJ) continue
-
-            const currCell = board[i][j]
-            //If cell alreay revealed continues
-            if (currCell.isRevealed) continue
-
-            const elCurrCell = document.querySelector(`.cell-${i}-${j}`)
-            currCell.isRevealed = true
-            gGame.revealedCount++
-            var strHTML = ''
-            strHTML += `${(currCell.isMine) ? BOMB_IMG : currCell.minesAroundCount} `
-            elCurrCell.innerHTML = strHTML
-
-        }
-    }
-
-}
-
-function checkGameOver(){
-    
-}
-
-function createCell() {
-    var cell = {}
-    cell.minesAroundCount = 4
-    cell.isRevealed = false
-    cell.isMine = false
-    cell.isMarked = false
-
-    return cell
 }
 
 function updateScore(diff) {
@@ -203,21 +103,4 @@ function handleWin() {
 
     const elModal = document.querySelector(".win")
     elModal.style.display = 'block'
-}
-
-function checkGameOver() {
-    for (var i = 0; i < size; i++) {
-        for (var j = 0; j < size; j++) {
-            currCell=gBoard[i][j]
-            if (currCell.isMarked){
-                if (!currCell.isMine) 
-                    return false
-            }
-        }
-    }
-    return true
-}
-
-function checkGameOver() {
-
 }
--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 function renderBoard(mat, selector) {
 
     var strHTML = '<table><tbody>'
@@ -11,35 +10,35 @@ function renderBoard(mat, selector) {
 
             const cell = mat[i][j]
             const className = `cell cell-${i}-${j}`
-            strHTML += `<td 
-            class="${className}" 
-            onclick="onCellClicked(this,${i},${j})"
-            oncontextmenu="onCellMarked(event,this, ${i}, ${j})">
-            <span></span>
-             </td>`
+
+            strHTML += `<td class="${className}">${cell}</td>`
         }
         strHTML += '</tr>'
     }
     strHTML += '</tbody></table>'
-
+    
     const elContainer = document.querySelector(selector)
     elContainer.innerHTML = strHTML
 }
 
-
-
-
+// pos is an object like this - { i: 2, j: 7 }
+function renderCell(pos, value) {
+    // Select the elCell and set the value
+    const elCell = document.querySelector(`.cell-${pos.i}-${pos.j}`)
+    elCell.innerHTML = value
+}
 
 function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-function getRandomHexColor() {
-    // Generate a random number between 0 and 16777215 (FFFFFF in hex).
-    // Convert it to a hexadecimal string.
-    let color = Math.floor(Math.random() * 16777215).toString(16);
 
-    // Pad the string with leading zeros if it's less than 6 characters long
-    // to ensure a valid 6-digit hex code.
-    return "#" + color.padStart(6, '0');
+function getRandomHexColor() {
+  // Generate a random number between 0 and 16777215 (FFFFFF in hex).
+  // Convert it to a hexadecimal string.
+  let color = Math.floor(Math.random() * 16777215).toString(16);
+
+  // Pad the string with leading zeros if it's less than 6 characters long
+  // to ensure a valid 6-digit hex code.
+  return "#" + color.padStart(6, '0');
 }
